9037677b7b7cf6eab499ea8f61ba01bbbd4a7a90
adrienn <raduly.adrienn@student.ms.sapientia.ro>
2025-07-08 12:21:33 +0300

    Vite-Vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <router-view /> <!-- This renders the matched component -->
+  </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style>
+/* Optional: You can add some global styles if necessary */
+</style>
